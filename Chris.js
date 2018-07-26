4ed7a1eae6a3fa6ba282b6df7051e7a228f5c1b2
ChristopherASC5 <chrisander15377@gmail.com>
2018-07-26 14:39:59 -0400

    Version 2

---
+++ Chris.js
@@ -0,0 +1,8 @@
+let ps4 = 299.99;
+let godOfWar = 59.99;
+let fortnite = 59.99;
+let detriotBecomeHuman = 59.99;
+
+const total = ps4 + godOfWar + fortnite + detriotBecomeHuman;
+
+console.log(total);
